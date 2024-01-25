6d5acda2e2fffd7b110dc29925af93c36371c2c3
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2024-01-25 13:10:27 +0300

    show popular posts on main page

--- blog/views.py
+++ blog/views.py
@@ -27,9 +27,13 @@ def serialize_tag(tag):
     }
 
 
-def index(request):
+def get_likes(post):
+    return post.likes.count()
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+def index(request):
+    all_posts = Post.objects.all()
+    sorted_posts = sorted(all_posts, key=get_likes)
+    most_popular_posts = sorted_posts[-5:]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
