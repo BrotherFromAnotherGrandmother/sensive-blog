90661348843e6826734911abf1e8c6a487faaf06
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2024-02-05 17:16:14 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -26,7 +26,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.all().annotate(posts_by_tag=Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -34,7 +34,7 @@ def serialize_post_optimized(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': tag.posts_by_tag,
     }
 
 
@@ -50,7 +50,7 @@ def index(request):
 
     most_fresh_posts = list(fresh_posts)[-5:]
     most_popular_posts = posts.popular().prefetch_related('author')[:5].fetch_with_comments_count()
-    most_popular_tags = Tag.objects.popular()[:5]
+    most_popular_tags = Tag.objects.popular().annotate(posts_by_tag=Count('posts'))[:5]
 
     context = {
         'most_popular_posts': [
