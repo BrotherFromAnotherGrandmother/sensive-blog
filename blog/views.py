a3bf4a298e63f47d6be585c771db26d68b963cf3
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2024-02-11 14:46:07 +0300

    cleanup the code

--- blog/views.py
+++ blog/views.py
@@ -26,26 +26,12 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag_updated(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
 
-def serialize_tag_updated(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': tag.posts_by_tag,
-    }
-
-
 def serialize_tag(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': tag.posts_by_tag,
-    }
-
-
-def serialize_tag_for_tag_filter(tag):
     return {
         'title': tag.title,
         'posts_with_tag': tag.posts_by_tag,
@@ -81,7 +67,7 @@ def index(request):
             serialize_post_optimized(post) for post in most_popular_posts
         ],
         'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
-        'popular_tags': [serialize_tag_updated(tag) for tag in most_popular_tags],
+        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
 
@@ -116,12 +102,12 @@ def post_detail(request, slug):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag_for_post_detail(tag) for tag in related_tags],
+        'tags': [serialize_tag(tag) for tag in related_tags],
     }
 
     context = {
         'post': serialized_post,
-        'popular_tags': [serialize_tag_for_post_detail(tag) for tag in most_popular_tags],
+        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
         'most_popular_posts': [
             serialize_post(post) for post in most_popular_posts
         ],
@@ -140,7 +126,7 @@ def tag_filter(request, tag_title):
 
     context = {
         'tag': tag.title,
-        'popular_tags': [serialize_tag_for_tag_filter(tag) for tag in most_popular_tags],
+        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
         'posts': [serialize_post(post) for post in related_posts],
         'most_popular_posts': [
             serialize_post(post) for post in most_popular_posts
